d65a7b1735bb9bd4bdce0a852281ba33039bedf1
JavierM333 <11445jam@gmail.com>
2025-04-27 16:22:01 -0400

    Initial Commit

---
+++ UFLib TrackerTests/UFLib_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  UFLib_TrackerTests.swift
+//  UFLib TrackerTests
+//
+//  Created by Javieralejandro Martinez on 4/27/25.
+//
+
+import Testing
+@testable import UFLib_Tracker
+
+struct UFLib_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
